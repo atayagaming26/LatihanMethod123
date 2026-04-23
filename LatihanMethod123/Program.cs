fd6f121c6195e0269cf865a013101d2b2d7614eb
atayagaming26 <atayagaming26@gmail.com>
2026-04-23 10:10:03 +0700

    LatihanMethod1Dan2

--- LatihanMethod123/Program.cs
+++ LatihanMethod123/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void tambah(int a, int b)
+{
+    int c = 0;
+
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+
+
+static void kurang(int a, int b)
+{
+    int c = 0;
+
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+
+
+
+static void kali(int a, int b)
+{
+    int c = 0;
+
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+
+
+static void bagi(int a, int b)
+{
+    int c = 0;
+
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+tambah(10, 2);
+
+tambah(8, 9);
+
+kurang(10, 2);
+
+kurang(90, 3);
+
+kali(10, 2);
+
+kali(8, 9);
+
+bagi(10, 2);
+
+bagi(8, 6);
